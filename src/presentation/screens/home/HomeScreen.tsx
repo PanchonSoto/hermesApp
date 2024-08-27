d29713a6badb1e4ff42c78e22e4ebad366ae30b8
Francisco Soto <panchon.ues@gmail.com>
2024-08-27 05:04:28 -0700

    remove view

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,6 +1,6 @@
 import { View, Text } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
-import { colors } from '../../../config/theme/theme';
+
 import { CustomView } from '../../components/ui/CustomView';
 import { Title } from '../../components/ui/Title';
 import { Button } from '../../components/ui/Button';
@@ -10,36 +10,31 @@ import { ThemeContext } from '../../context/ThemeContext';
 
 export const HomeScreen = () => {
 
-  const {setTheme, currentTheme, colors} = useContext(ThemeContext);
+  const { setTheme, currentTheme, colors } = useContext(ThemeContext);
 
 
   return (
-    <View>
-      <Text>HomeScreen</Text>
-      <Icon name="logo-react" size={30} color="#4F8EF7" />
-
-      <CustomView >
-      <Title text={`Cambiar thema: ${currentTheme}`} safe/>
+    <CustomView style={{padding:5}}>
+      <Title text={`Cambiar thema: ${currentTheme}`} safe />
 
       <Button
         text="light"
-        onPress={()=>setTheme('light')}
+        onPress={() => setTheme('light')}
       />
 
-      <View style={{height:10}}/>
+      <View style={{ height: 10 }} />
 
       <Button
         text="dark"
-        onPress={()=>setTheme('dark')}
+        onPress={() => setTheme('dark')}
       />
 
-      <View style={{height:10}}/>
+      <View style={{ height: 10 }} />
 
-      <Text style={{color: colors.text}}>
+      <Text style={{ color: colors.text }}>
         {JSON.stringify(colors, null, 2)}
       </Text>
 
     </CustomView>
-    </View>
   );
 }
