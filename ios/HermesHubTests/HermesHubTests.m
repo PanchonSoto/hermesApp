2a6b0ece2c97c869528d1f25733156fd6501f9bc
Francisco Soto <panchon.ues@gmail.com>
2024-10-04 11:05:53 -0700

    ios project name error && Gemfile update

--- ios/HermesHubTests/HermesHubTests.m
+++ ios/HermesHubTests/HermesHubTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface reactNativeTemplateTests : XCTestCase
+@interface HermesHubTests : XCTestCase
 
 @end
 
-@implementation reactNativeTemplateTests
+@implementation HermesHubTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
